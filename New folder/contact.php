b1c68f74252881eed58969e4e0b1816b7cd0c5d1
Chinmaya Kinnarkar <kinnarkarchinmaya@gmail.com>
2017-05-28 17:31:54 +0530

    Add files via upload

---
+++ New folder/contact.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" type="text/css" href="css/abc.css">
+   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet"> 
+	<title>Contact</title>
+	</head>
+<body>
+<header>
+	<div class="container">
+	 <div id="branding">
+	 	<h1><span class="highlight">Chinmaya Kinnarkar Portfolio</span></h1>
+	 </div>	
+	 <nav>
+	 	<ul>
+	 		<li><a href="p3.html">Home</a></li>
+	 		<li ><a href="about.html">About</a></li>
+	 		<li><a href="projects.html">Project</a></li>
+	 		<li class="current"><a href="contact.html">Contact</a></li>
+	 	</ul>
+	 </nav>
+	</div>
+</header>
+
+
+<section id="main">
+ <div class="container">
+  <h1 class="page-title">Contact</h1>
+<div class="contact">
+<p>Interested to hire me? That's great! Give me a call or send me an email and I will get back to you as soon as possible!</p>
+<form action="mail_handler.php" method="post" name="form" class="contact-me">
+ <div>
+ 	<label for="name">Name</label><br>
+ 	<input type="text" name="name" class="inp" placeholder="Name">
+ </div>
+<div>
+	<label for="email">Email</label><br>
+	<input type="email" name="email" class="inp" placeholder="Email Address">
+</div>
+<div>
+  <label for="message">Message</label><br>
+  <textarea name="msg" class="msg-box" placeholder="Message"></textarea><br>
+ 
+  <button name="submit" class="button_1" type="submit">Send</button>
+ </div>
+
+</form>	
+</div>
+</div>
+</section>
+
+<footer>
+	<p>&copy;2017 ChinmayaKinnarkar.All rights reserved.</p>
+</footer>
+</body>
+</html>
+
